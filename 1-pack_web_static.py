5792fe469cedfa75ddd4af48c307ff3cbe6ecdcf
Real-Sello <spmoneatse@gmail.com>
2023-09-07 21:05:42 +0200

    1. Compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that compresses files into .tgz format"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """Generates a .tgz archive"""
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_name = "versions/web_static_{}.tgz".format(timestamp)
+
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
+    except Exception as e:
+        print("Error:", str(e))
+        return None
+
+if __name__ == "__main__":
+    result = do_pack()
+    if result:
+        print("Archive created:", result)
+    else:
+        print("Archive creation failed.")
+
